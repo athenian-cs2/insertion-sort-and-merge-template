152b28e1803fcf0732c9a4675bc37ad25c099897
sthompson-athenian <sthompson@athenian.org>
2025-08-22 16:06:32 -0700

    Update Gradle to 7.6, Java to 17, and apply code formatting

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,21 +2,21 @@ import java.util.ArrayList;
 
 public class MyMain {
 
-    // Sorts the ArrayList using insertion sort
-    public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
-    }
+  // Sorts the ArrayList using insertion sort
+  public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
+    // YOUR CODE HERE
+    return null;
+  }
 
-     // Merges two sorted arrays into one large combined
-     // sorted array
-     // You may assume arr1 and arr2 are the same length
-    public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
-    }
+  // Merges two sorted arrays into one large combined
+  // sorted array
+  // You may assume arr1 and arr2 are the same length
+  public static int[] merge(int[] arr1, int[] arr2) {
+    // YOUR CODE HERE
+    return null;
+  }
 
-    public static void main(String[] args) {
-        // You can test your code here
-    }
+  public static void main(String[] args) {
+    // You can test your code here
+  }
 }
